a762a1ebf88e822d13cb15e2600420842088cc1e
rersoy76-source <rersoy76@gmail.com>
2026-03-08 16:40:24 +0300

    refactor distance functions with minkowski and add euclidean test

---
+++ tests/test_euclidean.py
@@ -0,0 +1,9 @@
+from utils.distances import euclidean
+
+def test_euclidean():
+    assert euclidean([0,0], [3,4]) == 5
+    from utils.distances import euclidean
+
+def test_euclidean():
+    assert euclidean([0,0], [3,4]) == 5
+    
--- utils/distances.py
+++ utils/distances.py
@@ -1,1 +1,17 @@
-pass  # YOUR CODE HERE
+def minkowski(a, b, p):
+    if len(a) != len(b):
+        raise ValueError("Inputs must have the same length")
+
+    total = 0
+    for x, y in zip(a, b):
+        total += abs(x - y) ** p
+
+    return total ** (1 / p)
+
+
+def manhattan(a, b):
+    return minkowski(a, b, 1)
+
+
+def euclidean(a, b):
+    return minkowski(a, b, 2)
